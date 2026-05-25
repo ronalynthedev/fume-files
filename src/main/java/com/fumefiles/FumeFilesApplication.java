ee7f8e09b6dac20ca896d567251f1aa56e5f1392
Ronalyn Nanong <waternanay@gmail.com>
2026-05-25 14:05:51 +0100

    initial commit: scaffolding - springboot and postgres setup

---
+++ src/main/java/com/fumefiles/FumeFilesApplication.java
@@ -0,0 +1,13 @@
+package com.fumefiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FumeFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FumeFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fumefiles/FumeFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fumefiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FumeFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
